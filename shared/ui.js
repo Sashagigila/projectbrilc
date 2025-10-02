605781378a141d666458e442e6112bbcbf150aea
sashagigila <2006aminev@gmail.com>
2025-10-02 09:33:15 +0500

    projectbrilc1

--- shared/ui.js
+++ shared/ui.js
@@ -1,14 +1,14 @@
 
 
 
-export const Button = ({children}) =>{
-return(
+export const Button = ({ children }) => {
+  return (
     <div className="bg-[#4d86ff] px-[20px] py-1 w-fit rounded-full transition duration-700 ease-in-out cursor-pointer hover:bg-[#8599f1]">
-        <a href="#">
-          <button>
-            <p className="text-white text-sm">{children}</p>
-          </button>
-        </a>
+      <a href="#">
+        <button>
+          <p className="text-white text-sm font-[Montserrat] font-semibold">{children}</p>
+        </button>
+      </a>
     </div>
-)
-}
+  );
+};
